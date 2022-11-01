92e7a000e5d6d4b620a9ecc03232d2f9921e6702
Manato HIRABAYASHI <manato.hirabayashi@tier4.jp>
2022-11-01 14:04:22 +0900

    feat: support getter to access getNbBindings result

--- src/tensorrt_common.cpp
+++ src/tensorrt_common.cpp
@@ -214,6 +214,10 @@ nvinfer1::Dims TrtCommon::getBindingDimensions(const int32_t index) const
   return context_->getBindingDimensions(index);
 }
 
+int32_t TrtCommon::getNbBindings()
+{
+  return engine_->getNbBindings();
+}
 
 bool TrtCommon::setBindingDimensions(const int32_t index, const nvinfer1::Dims & dimensions) const
 {
